035c74605227bdbc6f57ed032577015b7d79a36f
Dieter Stockhausen <dieter@schwingenhausen.at>
2021-04-11 06:49:38 +0200

    Fix install.bat

--- install.bat
+++ install.bat
@@ -3,5 +3,5 @@ set SCRIPT_DIR=%~dp0
 set VERSION=1.0.0
 ::
 cd /d %SCRIPT_DIR%target
-powershell -command "Expand-Archive -Force 'LRKomoot%VERSION%_win.zip' '%HOMEDRIVE%%HOMEPATH%"
+powershell -command "Expand-Archive -Force 'LRKomoot%VERSION%_win.zip' '%HOMEDRIVE%%HOMEPATH%'"
 
